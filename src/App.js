60bbd7a097804dd9183731400b51d36a08cca84d
coding-to-music <connors.tom@gmail.com>
2022-06-13 03:24:23 +0000

    Build

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ export default function App() {
       <RequireLoggedInUser>
         <RealmApolloProvider>
           <TaskApp />
+          <TaskApp />
         </RealmApolloProvider>
       </RequireLoggedInUser>
     </RealmAppProvider>
