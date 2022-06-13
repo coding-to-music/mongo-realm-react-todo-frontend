1da9bbcacaf85f43d7af984634abb4613a38de9e
coding-to-music <connors.tom@gmail.com>
2022-06-13 04:09:29 +0000

    Build

---
+++ src/graphql/useStreets.js
@@ -0,0 +1,40 @@
+import { useRealmApp } from "../RealmApp";
+import React from "react";
+
+// TODO: Retrieve the current user's streets and assign it to `streets`
+// with refresh when their street list is updated.
+
+function setStreetsFromChange(change, setStreets) {
+  const {
+    fullDocument: { memberOf },
+  } = change;
+  setStreets(memberOf);
+}
+
+export default function useStreets() {
+  const app = useRealmApp();
+  const [streets, setStreets] = React.useState(
+    app.currentUser.customData.memberOf
+  );
+  if (!app.currentUser) {
+    throw new Error("Cannot list streets if there is no logged in user.");
+  }
+  const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+  const users = mongodb.db("tracker").collection("User");
+
+  // set asynchronous event watcher to react to any changes in the users collection
+  React.useEffect(() => {
+    let changeWatcher;
+    (async () => {
+      changeWatcher = users.watch();
+      for await (const change of changeWatcher) {
+        setStreetsFromChange(change, setStreets);
+      }
+    })();
+
+    // close connection when component unmounts
+    return () => changeWatcher.return();
+  });
+
+  return streets;
+}
